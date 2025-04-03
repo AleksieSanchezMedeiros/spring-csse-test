11126157cec7a2e8625addb66b94bdeb6907e40a
AleksieSanchezMedeiros <aleksieisboss@gmail.com>
2025-04-03 14:49:11 -0700

    need to fix loop for reduce test

--- main.cpp
+++ main.cpp
@@ -26,9 +26,9 @@ void pow2Test() {
     }
     //if 7 then passed
     if (numOfPowers == 7) {
-        printf("POWER OF 2 TEST LOOP PASSED\n");
+        printf("Power Of 2 Test Loop PASSED\n");
     }else {
-        printf("POWER OF 2 TEST LOOP FAILED\n");
+        printf("Power Of 2 Test Loop FAILED\n");
     }
 
     //reset for second test
@@ -39,9 +39,9 @@ void pow2Test() {
     }
     //if 7 then passed
     if (numOfPowers == 7) {
-        printf("POWER OF 2 TEST NON LOOP PASSED\n");
+        printf("Power Of 2 Test Non Loop PASSED\n");
     }else {
-        printf("POWER OF 2 TEST NON LOOP FAILED\n");
+        printf("Power Of 2 Test Non Loop FAILED\n");
     }
 }
 
@@ -67,28 +67,49 @@ void primeTest() {
         numOfPrime += isNumPrime(i);
     }
     if (numOfPrime == 25) {
-        printf("PRIME NUM TEST PASSED\n");
+        printf("Prime Num Test PASSED\n");
     }else {
-        printf("PRIME NUM TEST FAILED\n");
+        printf("Prime Num Test FAILED\n");
     }
 }
 
+//fraction class for fraction reducer
+class Fraction {
+    public:
+        int numerator;
+        int denominator;
+
+    Fraction(int numerator, int denominator) {
+        this->numerator = numerator;
+        this->denominator = denominator;
+    }
+
+    std::string to_string() {
+        return std::to_string(numerator) + "/" + std::to_string(denominator);
+    }
+
+    bool equals(Fraction other) {
+        return numerator == other.numerator && denominator == other.denominator;
+    }
+};
+
 //reduce the fraction
-void reduceFrac(int numerator, int denominator) {
+Fraction reduceFrac(Fraction input) {
     //make numerator positive if denom is neg
-    if(denominator < 0){
-        numerator *= -1;
-        denominator *= -1;
+    //copied over from CSS143 fraction assignment
+    if(input.denominator < 0){
+        input.numerator *= -1;
+        input.denominator *= -1;
     }
 
     int numberToDivide = 2;
     //continue until the break or until the denominator is 1
-    while(numerator != 1 && denominator!= 1){
+    while(input.numerator != 1 && input.denominator!= 1){
         //divide by the current prime number, will repeat until no longer able to then moves to the next prime number
         //i think this could be a switch statement, but i don't really know how to use those
-        if(numerator % numberToDivide == 0 && denominator % numberToDivide == 0){
-            numerator = numerator / numberToDivide;
-            denominator = denominator / numberToDivide;
+        if(input.numerator % numberToDivide == 0 && input.denominator % numberToDivide == 0){
+            input.numerator = input.numerator / numberToDivide;
+            input.denominator = input.denominator / numberToDivide;
         } else if(numberToDivide == 2){
             numberToDivide = 3;
         } else if(numberToDivide == 3){
@@ -99,10 +120,38 @@ void reduceFrac(int numerator, int denominator) {
             break;
         }
     }
+    return input;
+}
+
+//reduce fraction tester, tests 3 different cases, no edge cases
+void fractionReduceTest() {
+    Fraction inputs[4] = {Fraction(1, 2),
+                        Fraction(2, 4),
+                        Fraction(4, 2),
+                        Fraction(6, 8)};
+
+    Fraction outputs[4] = {Fraction(1, 2),
+                        Fraction(1, 2),
+                        Fraction(2, 1),
+                        Fraction(3, 4)};
+
+    //has to stop at 4, no clue why
+    for (int i = 0; i < 4; i++) {
+        Fraction input = inputs[i];
+        Fraction expected = outputs[i];
+        Fraction actual = reduceFrac(input);
+
+        if(!expected.equals(actual)) {
+            std::cout << "Tests failed, current case is: \n" << inputs[i].to_string() << std::endl;
+        }
+    }
+
+    std::cout << "Tests passed!" << std::endl;
 }
 
 //runs test methods
 int main() {
     pow2Test(); //tests loop and non loop at the same time
     primeTest();
+    fractionReduceTest();
 }
